e3416ab8821e066111744bacebf08e12b5522774
Miguel Soto <miguelsoto@Miguels-MacBook-Pro.local>
2023-02-08 19:51:53 -0600

    Player movement Fixed

--- Bolitas/Assets/Scripts/PlayerController.cs
+++ Bolitas/Assets/Scripts/PlayerController.cs
@@ -2,40 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public float jumpAmount;
-    float inputMovement;
+    public float jumpSpeed;
+
+    bool isGrounded;
 
     Rigidbody2D rb;
-
-    bool isOnGround;
-    public Transform groundCheck;
-    public LayerMask groundMask;
-
-    public float radius;
     // Start is called before the first frame update
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();    
+        
     }
 
-    private void FixedUpdate() 
-    {
-        isOnGround = Physics2D.OverlapCircle(groundCheck.position, radius, groundMask);
-
-        inputMovement = Input.GetAxis("Horizontal"); //arrow key and "A" and "D"
-        rb.velocity = new Vector2(inputMovement * speed, rb.velocity.y);
-
-    }
     // Update is called once per frame
     void Update()
     {
-        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rb.velocity = Vector2.up * jumpAmount;
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            //salto sencillo
+            if(CheckGround.isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            }
         }
     }
 }
